807613e7b7c1809cb631d90fc8691c812405f7c5
majchrow <dmajchrowski6@gmail.com>
2020-03-22 02:38:05 +0100

    init parsing matrix ops

--- lab2/parser.py
+++ lab2/parser.py
@@ -13,15 +13,17 @@ def create_dir(directory):
 class Parser(object):
     tokens = Lexer.tokens + ('UMINUS',)
 
-    precedence = (  # TODO Fill
+    precedence = (
         ('nonassoc', 'IFX'),
         ('nonassoc', 'ELSE'),
+        ('nonassoc', 'ASSIGN', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
         ('nonassoc', 'GE', 'GEQ', 'LE', 'LEQ', 'EQ', 'NEQ'),
-        ('left', 'ADD', 'SUB', 'COMMA'),
-        ('left', 'MUL', 'DIV'),
+        ('left', 'ADD', 'SUB', 'DOTADD', 'DOTSUB', 'COMMA'),
+        ('left', 'MUL', 'DIV', 'DOTMUL', 'DOTDIV'),
         ('right', 'UMINUS'),
-        ('left', 'LPAREN', 'RPAREN'),
+        ('left', 'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET'),
         ('left', 'LCURLY', 'RCURLY'),
+
     )
 
     def __init__(self, start="statements", outputdir="logs", tabmodule="baseparsetab"):
@@ -50,27 +52,29 @@ class Parser(object):
                       | statement statements
         """
 
-    def p_statement(self, p):
-        """statement : IF expression block %prec IFX
-                     | IF expression block ELSE block
-                     | WHILE expression block
-                     | FOR for_expression block
-                     | PRINT print_expression SEMICOL
-                     | expressions SEMICOL
-        """
-
     def p_block(self, p):
         """block : statement
                  | LCURLY statements RCURLY
         """
 
-    def p_expressions(self, p):
-        """expressions : assign_expression
-                       | control_expression
+    def p_statement(self, p):
+        """statement : IF boolean_expression block %prec IFX
+                     | IF boolean_expression block ELSE block
+                     | WHILE boolean_expression block
+                     | FOR for_expression block
+                     | PRINT print_expression SEMICOL
+                     | assign_id SEMICOL
+                     | control_expression SEMICOL
         """
 
-    def p_expr_uminus(self, p):
-        """expression : SUB expression %prec UMINUS"""
+    def p_boolean_expression(self, p):
+        """boolean_expression : term GE term
+                              | term GEQ term
+                              | term LE term
+                              | term LEQ term
+                              | term EQ term
+                              | term NEQ term
+        """
 
     def p_for_expression(self, p):
         """ for_expression : ID ASSIGN idnum RANGE idnum"""
@@ -86,8 +90,19 @@ class Parser(object):
                              | print_expression COMMA print_expression
         """
 
-    def p_assign_expression(self, p):
-        """assign_expression : ID assign_op expression
+    def p_assign_id_factor(self, p):
+        """assign_id : ID ASSIGN factor
+                     | ID ADDASSIGN factor
+                     | ID SUBASSIGN factor
+                     | ID MULASSIGN factor
+                     | ID DIVASSIGN factor
+        """
+
+    def p_assign_id_matrix(self, p):
+        """assign_id : ID ASSIGN matrix_expression
+                     | ID ASSIGN matrix
+                     | ID ASSIGN ID TRANS
+                     | ID LBRACKET INTNUM COMMA INTNUM RBRACKET ASSIGN term
         """
 
     def p_control_expression(self, p):
@@ -96,39 +111,18 @@ class Parser(object):
                               | RETURN INTNUM
         """
 
-    def p_expression(self, p):
-        """expression : expression op expression
-                      | LPAREN expression RPAREN
-                      | term
+    def p_expression(self, p):  # It does not produce ID without expression
+        """expression : factor ADD factor
+                      | factor SUB factor
+                      | factor DIV factor
+                      | factor MUL factor
         """
 
-    def p_assign_op(self, p):
-        """assign_op : ASSIGN
-                     | ADDASSIGN
-                     | SUBASSIGN
-                     | MULASSIGN
-                     | DIVASSIGN
-        """
-
-    def p_op(self, p):
-        """op : bin_op
-              | rel_op
-        """
-
-    def p_bin_op(self, p):
-        """bin_op : ADD
-                  | SUB
-                  | DIV
-                  | MUL
-        """
-
-    def p_rel_op(self, p):
-        """rel_op : GE
-                  | GEQ
-                  | LE
-                  | LEQ
-                  | EQ
-                  | NEQ
+    def p_factor(self, p):
+        """factor : expression
+                  | LPAREN expression RPAREN
+                  | term
+                  | SUB factor %prec UMINUS
         """
 
     def p_term(self, p):
@@ -136,4 +130,37 @@ class Parser(object):
                 | INTNUM
                 | FLOATNUM
                 | UMINUS term
+        """
+
+    def p_matrix_special(self, p):
+        """matrix : EYE LPAREN INTNUM RPAREN
+                  | ZEROS LPAREN INTNUM RPAREN
+                  | ONES LPAREN INTNUM RPAREN
+        """
+
+    def p_matrix(self, p):
+        """matrix : LBRACKET matrix_rows RBRACKET
+        """
+
+    def p_matrix_rows(self, p):
+        """matrix_rows : matrix_rows SEMICOL matrix_row
+                       | matrix_row
+        """
+
+    def p_matrix_row(self, p):
+        """matrix_row : matrix_row COMMA term
+                      | term
+        """
+
+    def p_matrix_expression(self, p):
+        """matrix_expression : matrix_factor DOTADD matrix_factor
+                             | matrix_factor DOTSUB matrix_factor
+                             | matrix_factor DOTDIV matrix_factor
+                             | matrix_factor DOTMUL matrix_factor
+        """
+
+    def p_matrix_factor(self, p):
+        """matrix_factor : matrix_expression
+                         | LPAREN matrix_expression RPAREN
+                         | ID
         """
