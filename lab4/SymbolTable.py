a3d11f9bf1d7eb550b95f55ce751a2ea081a2334
majchrow <dmajchrowski6@gmail.com>
2020-05-12 16:58:11 +0200

    Add SymbolTable

--- lab4/SymbolTable.py
+++ lab4/SymbolTable.py
@@ -1,43 +1,34 @@
-#!/usr/bin/python
-
-class Symbol():
-    pass
+class Symbol(object):
+    def __init__(self, name, sym_type):
+        self.name = name
+        self.sym_type = sym_type
 
 
 class VariableSymbol(Symbol):
 
-    def __init__(self, name, type):
-        pass
-    #
+    def __init__(self, name, sym_type):
+        super().__init__(name, sym_type)
 
 
 class SymbolTable(object):
 
-    def __init__(self, parent, name):  # parent scope and symbol table name
-        pass
+    def __init__(self, parent: str, name: str):
+        self.scopes = []
+        self.symbols = {}
+        self.parent = parent
+        self.name = name
 
-    #
+    def put(self, name: str, symbol: Symbol):
+        self.symbols[name] = symbol
 
-    def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
-        pass
-
-    #
-
-    def get(self, name):  # get variable symbol or fundef from <name> entry
-        pass
-
-    #
+    def get(self, name: str):
+        return self.symbols[name]
 
     def getParentScope(self):
-        pass
+        return self.parent
 
-    #
-
-    def pushScope(self, name):
-        pass
-
-    #
+    def pushScope(self, name: str):
+        return self.scopes.append(name)
 
     def popScope(self):
-        pass
-    #
+        return self.scopes.pop()
